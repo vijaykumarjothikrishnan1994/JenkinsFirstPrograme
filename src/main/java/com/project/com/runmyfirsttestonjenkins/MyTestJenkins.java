9748b43a2ee11b90c2cce89e4849b5974b9a6265
vijaykumar <vijaykumar.js1994@gmail.com>
2021-05-16 20:35:43 +0530

    'test'

--- src/main/java/com/project/com/runmyfirsttestonjenkins/MyTestJenkins.java
+++ src/main/java/com/project/com/runmyfirsttestonjenkins/MyTestJenkins.java
@@ -1,13 +1,23 @@
 package com.project.com.runmyfirsttestonjenkins;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class MyTestJenkins {
+	WebDriver driver;
 
 	@Test
 	public void RunPom() {
 		
 		
 		System.out.println("This is my First programe");
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Vijay Krishnan\\eclipse-workspace\\com.runmyfirsttestonjenkins\\lib\\chromedriver.exe");
+		 driver=new ChromeDriver();
+			driver.manage().window().maximize();
+			
+			driver.navigate().to("http://www.seleniumframework.com/Practiceform/");
+			
 	}
 }
