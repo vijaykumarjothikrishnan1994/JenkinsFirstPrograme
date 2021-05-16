98fad7d8130bc67dbdc44f441a40ce93df2c0384
vijaykumar <vijaykumar.js1994@gmail.com>
2021-05-16 12:10:00 +0530

    'jenkinsfirstprograme'

---
+++ src/main/java/com/project/com/runmyfirsttestonjenkins/MyTestJenkins.java
@@ -0,0 +1,13 @@
+package com.project.com.runmyfirsttestonjenkins;
+
+import org.testng.annotations.Test;
+
+public class MyTestJenkins {
+
+	@Test
+	public void RunPom() {
+		
+		
+		System.out.println("This is my First programe");
+	}
+}
